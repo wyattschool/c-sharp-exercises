3b09b757889ac809038dcf07642986321bf39803
Wyatt School <wwebb@lakelandcollege.edu>
2022-04-29 19:43:33 -0500

    Create base classes.

---
+++ Restaurant/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Restaurant
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+
+		public class Menu
+		{
+
+		}
+
+		public class MenuItem
+		{
+			readonly string name;
+			static int price;
+			readonly string description;
+			readonly string category;
+			static string status;
+			static DateTime lastEditted;
+		}		
+	}
+}
