358683cf62b5589e050f7e9d09012ccb5afe1b9a
Wyatt School <wwebb@lakelandcollege.edu>
2022-04-30 20:48:24 -0500

    Add EditMenu class and methods to EditMenu class. Add DisplayMenu class and methods. Add contructors to MenuItem class.

--- Restaurant/Program.cs
+++ Restaurant/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Restaurant
 {
@@ -10,17 +11,64 @@ namespace Restaurant
 
 		public class Menu
 		{
-
+			public static List<MenuItem> Items = new List<MenuItem>();
+			public static List<string> CategoryList = new List<string>();
 		}
 
 		public class MenuItem
 		{
-			readonly string name;
-			static int price;
-			readonly string description;
-			readonly string category;
-			static string status;
-			static DateTime lastEditted;
-		}		
+			public string name;
+			public double price;
+			public string description;
+			public string category;
+			public string status;
+			public DateTime lastEditted;
+
+			public MenuItem(string itemName,double itemPrice, string itemDescription,string itemCategory, string itemStatus, DateTime whenLastEditted)
+			{
+				name = itemName;
+				price = itemPrice;
+				description = itemDescription;
+				category = itemCategory;
+				status = itemStatus;
+				lastEditted = whenLastEditted;
+			}
+		}
+
+		public class DisplayMenu
+		{
+			public void Display()
+			{
+				//Print items to be displayed
+			}
+
+			public void DisplayMenuOptions()
+			{
+				//Provide the user options as what of the menu they want displayed aka pick category if wanted
+			}
+		}
+
+		public class EditMenu
+		{
+			public static void CreateItem()
+			{
+				//Create new item
+			}
+
+			public static void AddItem()
+			{
+				//Add item to menu
+			}
+
+			public static void RemoveItem()
+			{
+				//Get item to remove and take it off the menu
+			}
+
+			public static void EradicateItem()
+			{
+				//Completely delete item from existing
+			}
+		}
 	}
 }
